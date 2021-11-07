80988a6faf5ce0695dc4f6e007aefcb6a34fd8aa
nguyenthanhthien <nguyenthanhthien2612@gmail.com>
2021-11-07 21:10:07 +0700

    import thu vien cho Bai Tong Hop

--- Bai_Tong_Hop.py
+++ Bai_Tong_Hop.py
@@ -1,1 +1,9 @@
 # Code chính viết ở đây hé
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_absolute_error
